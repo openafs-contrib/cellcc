2169a4509f568bc647d1c756ad47363a8ca1bc1a
Marcio Barbosa <mbarbosa@sinenomine.net>
2017-09-15 19:00:26 -0300

    SIGCHLD should not interrupt database connection
    During the XFER_WORK state, the restore-server forks a child process to
    fetch the dump file from the dump-server. Meanwhile, the parent process
    periodically updates the database with the amount of bytes already
    transferred. Since the monitor_child function does not ignore SIGCHLD,
    if the child process exits while the parent is communicating with the
    database, our empty SIGCHLD handler will run, potentially causing a
    syscall to fail with EINTR. This will most likely occur during the
    initial database connection, resulting in an error message that looks
    the following (the code '4' is EINTR):
    
    AFS.CellCC.Restore:ERROR: DBI connect([...]) failed: Can't connect to
    MySQL server on '[...]' (4)
    
    Typically, this scenario would happen when the volume is small enough
    (so the transfer does not take a long time) and the communication
    between the restore-server and the dump-server is faster than the
    communication between the restore-server and the database.
    
    To fix this problem, do not ignore SIGCHLD only when the parent process
    is sleeping.

--- lib/AFS/CellCC/Util.pm
+++ lib/AFS/CellCC/Util.pm
@@ -130,6 +130,16 @@ spawn_child(%) {
     return $pid;
 }
 
+# We can get interrupted by a SIGCHLD while sleeping
+sub
+_interruptible_sleep($) {
+    my ($sec) = @_;
+
+    local $SIG{CHLD} = sub {};
+
+    sleep($sec);
+}
+
 # Monitor a spawned child process. Takes a pid and some options in a hashref.
 # Available options:
 #  - name: A human-readable name for what we're waiting for; used in log messages
@@ -167,9 +177,6 @@ monitor_child($$) {
 
     my $last_cb = time();
 
-    # So we get interrupted by a SIGCHLD while sleeping
-    local $SIG{CHLD} = sub {};
-
     eval { while (1) {
         my $res = waitpid($pid, WNOHANG);
         if ($res < 0) {
@@ -200,7 +207,7 @@ monitor_child($$) {
             }
         }
 
-        sleep(1);
+        _interruptible_sleep(1);
     }};
     if ($@) {
         my $error = $@;
